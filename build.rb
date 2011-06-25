f95bed7e15a460c52cbafc770f8a34028f42cfdb
Anatol Pomozov <anatol.pomozov@gmail.com>
2011-06-24 17:33:37 -0700

    Add --clean flag

--- build.rb
+++ build.rb
@@ -3,12 +3,14 @@
 #   --debug       this builds distribuition with debug flags enabled
 #   --root DIR    install the binary into this directory. If this flag is not set - the script
 #                 redeploys kext to local machine and restarts it
+#   --clean       clean before build
 
 CWD = File.dirname(__FILE__)
 KEXT_DIR = '/System/Library/Extensions/'
 Dir.chdir(CWD)
 
 debug = ARGV.include?('--debug')
+clean = ARGV.include?('--clean')
 root_dir = ARGV.index('--root') ? ARGV[ARGV.index('--root') + 1] : nil
 
 abort("root directory #{root_dir} does not exist") if ARGV.index('--root') and not File.exists?(root_dir)
@@ -32,14 +34,11 @@ if File.exists?("../kext/build.rb") then
   end
 end
 
+system('git clean -xdf') if clean
+
 unless File.exists?('Makefile') then
   system("autoreconf -f -i -Wall,no-obsolete") or abort
   system("./configure --disable-dependency-tracking --disable-static") or abort
-end
-
-if root_dir
-  # In case if we build the final distribution - clean project to avoid possible stale changes
-  system('make clean')
 end
 
 system("make -s -j3") or abort
